a52d00215a3dcec60e518b51ec0219c95490292a
Tanveer Ahmed <tanveerahmed7890@gmail.com>
2020-12-29 22:44:50 +0600

    Add files via upload
    Designed The UI, Added Database, and Added Multiple User Type Login System.

---
+++ ShopChiefPro/src/shopchiefpro/JTable_import_and_export_to_text_file.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopchiefpro;
+
+/**
+ *
+ * @author Shinigami
+ */
+class JTable_import_and_export_to_text_file {
+    
+}
---
+++ ShopChiefPro/src/shopchiefpro/UserDatabase.java
@@ -0,0 +1,157 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shopchiefpro;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+/**
+ *
+ * @author Shinigami
+ */
+ class User{
+        String username;
+        String password;
+        String type;
+        public User(){
+            
+        }
+        public User(String username, String password, String type)
+        {
+            this.username = username;
+            this.password = password;
+            this.type = type;
+        }
+        public String getUsername()
+        {
+            return this.username;
+        }
+        public String getPassword()
+        {
+            return this.password;
+        }
+        
+        
+        
+        
+    };
+public class UserDatabase {
+    
+   
+    
+    User logged_user = null;
+    String file_name = "user_data";
+    ArrayList<User> users = new ArrayList<User>(); 
+    
+    public UserDatabase()
+    {
+        this.load_data();
+    }
+    
+    private void load_data()
+    {
+        try{
+       File f = new File("res/"+file_name+".txt");
+       Scanner fs = new Scanner(f);
+       
+       while(fs.hasNextLine()){
+           String line = fs.nextLine();
+           String[] as = line.split("\t");
+           
+           User user = new User(as[0], as[1], as[2]);
+           
+           this.users.add(user);
+       }
+       } catch(Exception e)
+       {
+           System.out.println("File Not FOUND!");
+       }
+    }
+    
+    private void save_data()
+    {
+         try {
+
+            FileWriter file = new FileWriter("res/"+file_name+".txt",false);
+
+            for (int i = 0; i < this.users.size(); i++) {
+                User user = this.users.get(i);
+                file.write(user.username+"\t" + user.password+"\t"+user.type + "\n");
+            }
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+    
+    public User get_user(String username)
+    {
+        for (int i = 0; i<this.users.size(); i++)
+        {
+            
+            if (this.users.get(i).getUsername().equals(username)){
+                return this.users.get(i);
+            }
+        }
+        
+        return null;
+    }
+    
+    public void make_user(String username, String password, String type){
+        
+        User user = new User(username, password, type); 
+        this.users.add(user);
+        this.save_data();
+        
+        
+    }
+    
+    public void delete_user(String username)
+    {
+        for (int i = 0; i<this.users.size(); i++)
+        {
+            
+            if (this.users.get(i).getUsername().equals(username)){
+                this.users.remove(this.users.get(i));
+                break;
+            }
+        }
+        this.save_data();
+    }
+    
+    public boolean check_logged_in()
+    {
+        return this.logged_user != null;
+        
+    }
+    
+    public User get_current_user(){
+        return this.logged_user;
+    }
+    
+    public User login(String username, String password){
+        User user = this.get_user(username);
+       if ( user != null){
+           if (user.getPassword().equals(password)){
+               this.logged_user = user;
+               return user;
+       }
+    }
+       return null;
+    }
+       
+    
+   
+    protected void finalize()
+    {
+        this.save_data();
+    }
+    
+    
+}
